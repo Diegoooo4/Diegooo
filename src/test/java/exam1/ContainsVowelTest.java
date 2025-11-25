797ed450e134380d7c368c264d48516b7b473f78
Diego Vizcaí Sesma <dievizses@edu.gva.es>
2025-11-25 13:11:40 +0100

    Test sin vocales

--- src/test/java/exam1/ContainsVowelTest.java
+++ src/test/java/exam1/ContainsVowelTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+<<<<<<< HEAD
 
 @Nested
 class ContainsVowelTest {
@@ -14,6 +15,16 @@ class ContainsVowelTest {
         //Arrange
         String str = "Hello world";
         boolean expected = true;
+=======
+@Nested
+class ContainsVowelTest {
+    @Test
+    @DisplayName("Text with out vowels mayus")
+    void ShouldRturnWithOutVowelsMays(){
+        //Arrange
+        String str = "HLL WRLD";
+        boolean expected = false;
+>>>>>>> 33123ce (Test sin vocales)
 
         //Act
         boolean actual = ContainsVowel.containsVowel(str);
@@ -22,6 +33,7 @@ class ContainsVowelTest {
         assertEquals(expected, actual);
     }
     @Test
+<<<<<<< HEAD
     @DisplayName("Text with vowels mayus")
     void ShouldRturnWithVowelsMays(){
         //Arrange
@@ -30,7 +42,22 @@ class ContainsVowelTest {
 
         //Act
         boolean actual = ContainsVowel.containsVowel(str);
+=======
+    @DisplayName("Text with out vowels minus")
+    void ShouldRturnWithOutVowels(){
+        //Arrange
+        String str = "Hll wrld";
+        boolean expected = false;
+
+        //Act
+        boolean actual = ContainsVowel.containsVowel(str);
+
+>>>>>>> 33123ce (Test sin vocales)
         //Assert
         assertEquals(expected, actual);
     }
 }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 33123ce (Test sin vocales)
